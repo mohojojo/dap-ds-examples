6e7b6a58e906f76e041985b76e05688d77454497
Adam Burucs <adam.burucs@trendency.hu>
2024-12-04 17:43:12 +0100

    feat(angular): AS-545 add combobox, search for templ driven, WIP

--- src/angular/form-example/src/app/template-driven/template-driven.component.ts
+++ src/angular/form-example/src/app/template-driven/template-driven.component.ts
@@ -6,11 +6,16 @@ import { DapDSCheckboxValueAccessorDirective } from '../directives/dap-ds-checkb
 import { DapDSDatePickerValueAccessorDirective } from '../directives/dap-ds-datepicker.directive'
 import { DapDSTextareaValueAccessorDirective } from '../directives/dap-ds-textarea.directive'
 import { DapDSSelectValueAccessorDirective } from '../directives/dap-ds-select.directive'
+import { Product } from '../model/product'
+import { debounceTime, Subject } from 'rxjs'
+import { ProductService } from '../service/product.service'
+import { DapDSComboboxAccessorDirective } from '../directives/dap-ds-combobox.directive'
 
 @Component({
   selector: 'app-template-driven',
   imports: [
     DapDSInputValueAccessorDirective,
+    DapDSComboboxAccessorDirective,
     DapDSCheckboxValueAccessorDirective,
     DapDSDatePickerValueAccessorDirective,
     DapDSTextareaValueAccessorDirective,
@@ -23,15 +28,35 @@ import { DapDSSelectValueAccessorDirective } from '../directives/dap-ds-select.d
 })
 export class TemplateDrivenComponent {
   submitted = false
+  products: Product[] = [];
+  private searchSubject = new Subject<string>();
 
   formData = {
     fullName: '',
     title: '',
     email: '',
     birthDate: '',
+    product: '',
     subject: '',
     message: '',
     tnc: false,
+  }
+
+  constructor(private productService: ProductService) {
+    this.searchSubject.pipe(debounceTime(300)).subscribe((productFilter: string) => {
+        console.log('Debounced value changed:', productFilter);
+        this.getProducts(productFilter);
+    });
+  }
+
+  getProducts(productFilter: string): void {
+    if (productFilter) {
+      this.productService.getProducts(productFilter).subscribe(searchResult => {
+        if (searchResult?.products) {
+          this.products = searchResult?.products;
+        }
+      });
+    }
   }
 
   onSubmit() {
