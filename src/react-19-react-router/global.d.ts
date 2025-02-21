25e5d9627afea6c776c36861b9bc44cb807fc500
mohojojo <mohojojoo@gmail.com>
2025-02-21 08:04:21 +0100

    add react 19 example

---
+++ src/react-19-react-router/global.d.ts
@@ -0,0 +1,72 @@
+declare namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        'dap-ds-accordion': import('dap-design-system/dist/react-types').DapDSAvatarType
+        'dap-ds-anchor-heading': import('dap-design-system/dist/react-types').DapDSAnchorHeadingType
+        'dap-ds-avatar': import('dap-design-system/dist/react-types').DapDSAnchorHeadingType
+        'dap-ds-banner': import('dap-design-system/dist/react-types').DapDSBannerType
+        'dap-ds-breadcrumb': import('dap-design-system/dist/react-types').DapDSBreadcrumbType
+        'dap-ds-accordion-group': import('dap-design-system/dist/react-types').DapDSAccordionGroupType
+        'dap-ds-badge': import('dap-design-system/dist/react-types').DapDSBadgeType
+        'dap-ds-breadcrumb-item': import('dap-design-system/dist/react-types').DapDSBreadcrumbItemType
+        'dap-ds-button': import('dap-design-system/dist/react-types').DapDSButtonType
+        'dap-ds-calendar-cell': import('dap-design-system/dist/react-types').DapDSCalendarCellType
+        'dap-ds-calendar': import('dap-design-system/dist/react-types').DapDSCalendarType
+        'dap-ds-callout': import('dap-design-system/dist/react-types').DapDSCalloutType
+        'dap-ds-card-actions': import('dap-design-system/dist/react-types').DapDSCardActionsType
+        'dap-ds-card-content': import('dap-design-system/dist/react-types').DapDSCardContentType
+        'dap-ds-card-image': import('dap-design-system/dist/react-types').DapDSCardImageType
+        'dap-ds-card-subtitle': import('dap-design-system/dist/react-types').DapDSCardSubtitleType
+        'dap-ds-card-title': import('dap-design-system/dist/react-types').DapDSCardTitleType
+        'dap-ds-card': import('dap-design-system/dist/react-types').DapDSCardType
+        'dap-ds-checkbox': import('dap-design-system/dist/react-types').DapDSCheckboxType
+        'dap-ds-combobox': import('dap-design-system/dist/react-types').DapDSComboboxType
+        'dap-ds-content-switcher-item': import('dap-design-system/dist/react-types').DapDSContentSwitcherItemType
+        'dap-ds-content-switcher': import('dap-design-system/dist/react-types').DapDSContentSwitcherType
+        'dap-ds-copybox-input': import('dap-design-system/dist/react-types').DapDSCopyBoxInputType
+        'dap-ds-dap-badge': import('dap-design-system/dist/react-types').DapDSBadgeType
+        'dap-ds-datepicker': import('dap-design-system/dist/react-types').DapDSDatePickerType
+        'dap-ds-divider': import('dap-design-system/dist/react-types').DapDSDividerType
+        'dap-ds-feedback': import('dap-design-system/dist/react-types').DapDSFeedbackType
+        'dap-ds-file-input-list-item': import('dap-design-system/dist/react-types').DapDSFileInputListItemType
+        'dap-ds-file-input-list': import('dap-design-system/dist/react-types').DapDSFileInputListType
+        'dap-ds-file-input': import('dap-design-system/dist/react-types').DapDSFileInputType
+        'dap-ds-icon': import('dap-design-system/dist/react-types').DapDSIconType
+        'dap-ds-icon-button': import('dap-design-system/dist/react-types').DapDSIconButtonType
+        'dap-ds-input': import('dap-design-system/dist/react-types').DapDSInputType
+        'dap-ds-modal': import('dap-design-system/dist/react-types').DapDSModalType
+        'dap-ds-list-item': import('dap-design-system/dist/react-types').DapDSListItemType
+        'dap-ds-number-input': import('dap-design-system/dist/react-types').DapDSNumberInputType
+        'dap-ds-option-item': import('dap-design-system/dist/react-types').DapDSOptionItemType
+        'dap-ds-link': import('dap-design-system/dist/react-types').DapDSLinkType
+        'dap-ds-option-list': import('dap-design-system/dist/react-types').DapDSOptionListType
+        'dap-ds-overlay': import('dap-design-system/dist/react-types').DapDSOverlayType
+        'dap-ds-password-input': import('dap-design-system/dist/react-types').DapDSPasswordInputType
+        'dap-ds-popup': import('dap-design-system/dist/react-types').DapDSPopupType
+        'dap-ds-radio-button': import('dap-design-system/dist/react-types').DapDSRadioButtonType
+        'dap-ds-search': import('dap-design-system/dist/react-types').DapDSSearchType
+        'dap-ds-select': import('dap-design-system/dist/react-types').DapDSSelectType
+        'dap-ds-sidenav': import('dap-design-system/dist/react-types').DapDSSideNavType
+        'dap-ds-sidenav-item': import('dap-design-system/dist/react-types').DapDSSideNavItemType
+        'dap-ds-sidenav-group': import('dap-design-system/dist/react-types').DapDSSideNavGroupType
+        'dap-ds-skip-link': import('dap-design-system/dist/react-types').DapDSSkipLinkType
+        'dap-ds-spinner': import('dap-design-system/dist/react-types').DapDSSpinnerType
+        'dap-ds-stack': import('dap-design-system/dist/react-types').DapDSStackType
+        'dap-ds-switch': import('dap-design-system/dist/react-types').DapDSSwitchType
+        'dap-ds-tab-group': import('dap-design-system/dist/react-types').DapDSTabGroupType
+        'dap-ds-tab': import('dap-design-system/dist/react-types').DapDSTabType
+        'dap-ds-table-cell': import('dap-design-system/dist/react-types').DapDSTableCellType
+        'dap-ds-table-header': import('dap-design-system/dist/react-types').DapDSTableHeaderType
+        'dap-ds-table-row': import('dap-design-system/dist/react-types').DapDSTableRowType
+        'dap-ds-table': import('dap-design-system/dist/react-types').DapDSTableType
+        'dap-ds-textarea': import('dap-design-system/dist/react-types').DapDSTextareaType
+        'dap-ds-toc': import('dap-design-system/dist/react-types').DapDSTOCType
+        'dap-ds-tray': import('dap-design-system/dist/react-types').DapDSTrayType
+        'dap-ds-tooltip': import('dap-design-system/dist/react-types').DapDSTooltipType
+        'dap-ds-typography': import('dap-design-system/dist/react-types').DapDSTypographyType
+      }
+    }
+  }
+    
+  declare module 'dap-design-system/dist/dds.js'
+    
